885fd5673edd2b20fb48c88182176b2ccfb76150
Oleg Koptev <olegstankoptev@gmail.com>
2022-01-14 12:51:28 +0300

    fin

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,43 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+    app.get("item-type") { req -> String in
+        do {
+            let data = try String(contentsOf:
+                URL(fileURLWithPath: app.directory.workingDirectory)
+                    .appendingPathComponent("item-type.json", isDirectory: false)
+            )
+            return data
+        } catch {
+            return error.localizedDescription
+        }
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    
+    app.get("item-type", ":item-type") { req -> String in
+        do {
+            let itemType = req.parameters.get("item-type")!
+            let data = try String(contentsOf:
+                URL(fileURLWithPath: app.directory.workingDirectory)
+                    .appendingPathComponent("item-type", isDirectory: true)
+                    .appendingPathComponent("\(itemType).json", isDirectory: false)
+            )
+            return data
+        } catch {
+            return error.localizedDescription
+        }
+    }
+    
+    app.get("item", ":item") { req -> String in
+        do {
+            let item = req.parameters.get("item")!
+            let data = try String(contentsOf:
+                URL(fileURLWithPath: app.directory.workingDirectory)
+                    .appendingPathComponent("item", isDirectory: true)
+                    .appendingPathComponent("\(item).json", isDirectory: false)
+            )
+            return data
+        } catch {
+            return error.localizedDescription
+        }
     }
 }
